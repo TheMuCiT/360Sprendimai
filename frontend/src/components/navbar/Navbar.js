334deb26074e513ce82d7bf431e6b07e7729d41e
THeMuCiT127 <themucit127@gmail.com>
2023-03-27 16:07:31 +0300

    This is a new commit for what I originally planned to be amended

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -2,66 +2,76 @@ import React, { useEffect, useState } from 'react'
 
 import { ReactComponent as MobileMenu } from '../../icons/MobileMenu.svg'
 import { ReactComponent as Close } from '../../icons/Close.svg'
+import { ReactComponent as MenuDesktop } from '../../icons/MenuDesktop.svg'
+import { ReactComponent as Logo } from '../../icons/Logo.svg'
 
 import './navbar.css'
 const Navbar = () => {
-     const [Mobile, setMobile] = useState(false)
-     useEffect(() => {
-          WindowChange()
-     }, [])
+  const [Mobile, setMobile] = useState(false)
+  useEffect(() => {
+    WindowChange()
+  }, [])
 
-     const HandleMobileMenu = () => {
-          setMobile(!Mobile)
-     }
+  const HandleMobileMenu = () => {
+    setMobile(!Mobile)
+  }
 
-     const WindowChange = () => {
-          if (window.innerWidth > 1050) {
-               setMobile(false)
-          }
-     }
+  const WindowChange = () => {
+    if (window.innerWidth > 1050) {
+      setMobile(false)
+    }
+  }
 
-     window.addEventListener('resize', WindowChange)
-     return (
-          <div className='navbar'>
-               <div className='navbarMobile'>
-                    <div className='navbarCenterIcon'>
-                         <div className='navbarMobileTopRight '>MobileLeftTitle</div>
-                    </div>
-               </div>
-               <div className='navbarMobileButton' >
-                    <MobileMenu className={Mobile ? 'Mobile' : 'Mobile'} onClick={HandleMobileMenu}/>
-                    <div className={Mobile ? 'navbarMobileContainerActive' : 'navbarMobileContainer'}>
-                         <div className={Mobile ? 'navbarMenu active' : 'navbarMenu'}>
-                              <div className='navbarMenuContainer'>
-                                   <div className='navbarMobileTop'>
-                                        <div className='navbarMobileTopRight menuOpen'>MobileMenuText</div>
-                                        <div className='navbarMobileTopLeft'>
-                                             <Close className='CloseIcon' onClick={HandleMobileMenu} />
-                                        </div>
-                                   </div>
-                                   <div className='navbarMobileMain'>
-                                        <div className='navbarCenterLink opacity7'>MobileMenuMiddleText</div>
-                                        <div className='navbarCenterLink navbarRightButton'>MobileMenuButton</div>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-               </div>
-
-               <div className='navbarContainer SlideRightAnimation'>
-                    <div className='navbarLeft'>
-                         <div className='navbarLeftLogo '>LeftNavbar</div>
-                    </div>
-                    <div className='navbarCenter'>
-                         <div className='navbarCenterItem'>MiddleNavbar</div>
-                    </div>
-                    <div className='navbarRight'>
-                         <div className='navbarRightItem'>RightNavbar</div>
-                         <div className='navbarRightButton'>NavbarButton</div>
-                    </div>
-               </div>
+  window.addEventListener('resize', WindowChange)
+  return (
+    <div className='navbar'>
+      <div className='navbarMobile'>
+        <div className='navbarCenterIcon'>
+          <div className='navbarMobileTopRight '>MobileLeftTitle</div>
+        </div>
+      </div>
+      <div className='navbarMobileButton'>
+        <MobileMenu className={Mobile ? 'Mobile' : 'Mobile'} onClick={HandleMobileMenu} />
+        <div className={Mobile ? 'navbarMobileContainerActive' : 'navbarMobileContainer'}>
+          <div className={Mobile ? 'navbarMenu active' : 'navbarMenu'}>
+            <div className='navbarMenuContainer'>
+              <div className='navbarMobileTop'>
+                <div className='navbarMobileTopRight menuOpen'>MobileMenuText</div>
+                <div className='navbarMobileTopLeft'>
+                  <Close className='CloseIcon' onClick={HandleMobileMenu} />
+                </div>
+              </div>
+              <div className='navbarMobileMain'>
+                <div className='navbarCenterLink opacity7'>MobileMenuMiddleText</div>
+                <div className='navbarCenterLink navbarRightButton'>MobileMenuButton</div>
+              </div>
+            </div>
           </div>
-     )
+        </div>
+      </div>
+
+      <div className='navbarContainer SlideRightAnimation'>
+        <div className='navbarLeft'>
+          <div className='navbarLeftLogo '>
+            <Logo />
+          </div>
+        </div>
+        <div className='navbarCenter'>
+          <div className='navbarCenterItem'>Home</div>
+          <div className='navbarCenterItem'>Pricing</div>
+          <div className='navbarCenterItem'>Project</div>
+          <div className='navbarCenterItem'>About Company</div>
+          <div className='navbarCenterItem'>Contact</div>
+        </div>
+        <div className='navbarRight'>
+          <div className='navbarRightItemCustom'>
+            <MenuDesktop />
+          </div>
+          <div className='navbarRightButton'>Book a call</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -1,14 +1,87 @@
 import React from 'react'
+
+import { ReactComponent as BookNow } from '../../icons/BookNow.svg'
+import { ReactComponent as StarIcon } from '../../icons/StarIcon.svg'
+
+import mainBC from '../../images/Main.webp'
+import Video360 from '../../images/360Video.png'
 import Animation from '../../components/animation/Animation'
 
 import './main.css'
 
 const Main = () => {
-     return (
+  return (
+    <div className='mainContainer'>
+      <img src={mainBC} alt='' className='mainImage' />
+      <div className='componentFirst' style={{ paddingTop: '150px' }}>
+        <div className='componentSecond'>
           <div className='main'>
-               <Animation />
+            <div className='mainLeft'>
+              <div className='mainTitle'>Property Marketing Excellence</div>
+              <div className='mainSubTitle'>
+                Photoplan Are Dedicated Professionals Who Love What We Do. Our Incredible Team Work Tirelessly To
+                Deliver Exceptional Service While Producing High-end Digital Projects For Commercial & Residential
+                Properties Throughout The UK And Worldwide!
+              </div>
+              <div className='mainButtons'>
+                <div className='mainButton'>Book a call</div>
+                <div className='mainButtonInner'>Read more</div>
+              </div>
+            </div>
+            <div className='line' />
+            <div className='mainRight'>
+              <div className='line' />
+            </div>
           </div>
-     )
+          <div className='main'>
+            <div className='mainLeftEmpty'></div>
+            <div className='line' />
+            <div className='mainRight'>
+              <div className='mainRightLeft'></div>
+              <div className='line' />
+              <div className='mainRightRight'>
+                <div className='mainRightIcon'>
+                  <BookNow />
+                </div>
+                <div className='mainRightText'>Book Now</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='componentFirst' style={{ backgroundColor: '#15181B' }}>
+        <div className='componentSecond' style={{ borderTop: 'none' }}>
+          <div className='main'>
+            <div className='mainLeft'>
+              <div className='mainTitle'>Property Marketing Excellence</div>
+              <div className='mainSubTitle'>
+                Photoplan Are Dedicated Professionals Who Love What We Do. Our Incredible Team Work Tirelessly To
+                Deliver Exceptional Service While Producing High-end Digital Projects For Commercial & Residential
+                Properties Throughout The UK And Worldwide!
+              </div>
+              <div className='mainButtons'>
+                <div className='mainButton'>Book a call</div>
+                <div className='mainButtonInner'>Read more</div>
+              </div>
+            </div>
+            <div className='line' />
+            <div className='mainRight' style={{ flexDirection: 'column' }}>
+              <div className='mainRightImage'>
+                <img src={Video360} alt='' style={{ width: '100%' }} />
+              </div>
+              <div className='mainRightGrid'>
+                <div className='mainRightGridItem'>asd</div>
+                <div className='mainRightGridItem'>asd</div>
+                <div className='mainRightGridItem'>asd</div>
+                <div className='mainRightGridItem'>asd</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Animation />
+    </div>
+  )
 }
 
 export default Main
