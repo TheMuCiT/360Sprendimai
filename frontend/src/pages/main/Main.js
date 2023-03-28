176b07e330206ac0db6d4f3d8ed4c3b542c375dc
THeMuCiT127 <themucit127@gmail.com>
2023-03-28 22:57:18 +0300

    main page

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -180,6 +180,107 @@ const Main = () => {
           </div>
         </div>
       </div>
+
+      <div className='componentFirst' style={{ backgroundColor: '#FFFFFF' }}>
+        <div className='componentSecond' style={{ borderTop: 'none' }}>
+          <div className='main'>
+            <div className='mainLeft mainLeftOversizeImage2' style={{ borderRight: '1px solid red' }}>
+              <div className='mainTitle' style={{ color: '#15181B', maxWidth: '350px' }}>
+                Subscription plans
+              </div>
+              <div className='mainSubTitle'>
+                Photoplan are dedicated professionals who love what we do. Our incredible team work tirelessly to
+                deliver exceptional service while producing high-end digital projects for commercial & residential
+                properties throughout the UK and Worldwide!
+              </div>
+              <div className='mainButtons'>
+                <div className='mainButton' style={{ backgroundColor: '#15181B', color: '#fff' }}>
+                  Book a call
+                </div>
+                <div className='mainButtonInner' style={{ borderColor: '#15181B', color: '#15181B' }}>
+                  Read more
+                </div>
+              </div>
+            </div>
+            <div className='mainRight' style={{ width: '100%' }}>
+              <div className='mainRightFull'>
+                <div className='mainRightFullItem'>
+                  <div className='mainRightFullItemLeft'>
+                    <div className='mainRightFullPlan'>Subscription plan</div>
+                    <div className='mainRightFullTitle'>IndividualX</div>
+                    <div className='mainRightFullOption'>Everything offered with invidual plan</div>
+                    <div className='mainRightFullOption'>Up to 100 m2 apartament</div>
+                    <div className='mainRightFullOption'>Service & Development</div>
+                  </div>
+                  <div className='mainRightFullItemRight'>€ 50.00</div>
+                </div>
+                <div className='mainRightFullItem'>
+                  <div className='mainRightFullItemLeft'>
+                    <div className='mainRightFullPlan'>Subscription plan</div>
+                    <div className='mainRightFullTitle'>TeamX</div>
+                    <div className='mainRightFullOption'>Everything offered with invidual plan</div>
+                    <div className='mainRightFullOption'>Up to 100 m2 apartament</div>
+                    <div className='mainRightFullOption'>Service & Development</div>
+                  </div>
+                  <div className='mainRightFullItemRight'>€ 100.00</div>
+                </div>
+                <div className='mainRightFullItem'>
+                  <div className='mainRightFullItemLeft'>
+                    <div className='mainRightFullPlan'>Subscription plan</div>
+                    <div className='mainRightFullTitle'>ProfessionalX</div>
+                    <div className='mainRightFullOption'>Everything offered with invidual plan</div>
+                    <div className='mainRightFullOption'>Up to 100 m2 apartament</div>
+                    <div className='mainRightFullOption'>Service & Development</div>
+                  </div>
+                  <div className='mainRightFullItemRight'>∞</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='componentFirst' style={{ backgroundColor: '#15181B' }}>
+        <div className='componentSecond' style={{ borderTop: 'none' }}>
+          <div className='main'>
+            <div className='mainLeft' style={{ backgroundColor: 'transparent' }}>
+              <div className='mainTitle' style={{ maxWidth: '300px' }}>
+                Let’s Collaborate
+              </div>
+              <div className='mainSubTitle'>
+                Photoplan Are Dedicated Professionals Who Love What We Do. Our Incredible Team Work Tirelessly To
+                Deliver Exceptional Service While Producing High-end Digital Projects For Commercial & Residential
+                Properties Throughout The UK And Worldwide!
+              </div>
+            </div>
+            <div className='line' />
+            <div
+              className='mainRight'
+              style={{ flexDirection: 'column', alignItems: 'flex-start', padding: '100px 0px' }}
+            >
+              <div className='mainRightPadding'>
+                <div className='mainEmailTitle'>Say, Hello!</div>
+                <div className='mainEmailSubTitle'>Full Name</div>
+                <div className='mainEmailInput'>
+                  <input type='text' className='mainEmailInputElement' placeholder='@Full Name' />
+                </div>
+
+                <div className='mainEmailSubTitle'>Email</div>
+                <div className='mainEmailInput'>
+                  <input type='text' className='mainEmailInputElement' placeholder='@Email' />
+                </div>
+
+                <div className='mainEmailSubTitle'>Message</div>
+                <div className='mainEmailInput'>
+                  <input type='text' className='mainEmailInputElement' placeholder='@Message' />
+                </div>
+
+                <div className='mainEmailSubmit'>Submit</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Animation />
     </div>
   )
